269d716a7b0cd897adc8f270badfce99b8d861da
Aleksey Mikhailik <amikhailik@positrace.com>
2013-01-16 15:58:56 +0200

    Separated config and requirejs tag

--- app/helpers/requirejs_helper.rb
+++ app/helpers/requirejs_helper.rb
@@ -19,6 +19,34 @@ module RequirejsHelper
       %Q{data-#{k}="#{v}"}
     end.join(" ")
   end
+  
+  def requirejs_config(requirejs_config)
+    unless requirejs_config.run_config.empty?
+      run_config = requirejs_config.run_config.dup
+      unless _priority.empty?
+        run_config = run_config.dup
+        run_config[:priority] ||= []
+        run_config[:priority].concat _priority
+      end
+      if Rails.application.config.assets.digest
+        modules = requirejs_config.build_config['modules'].map { |m| requirejs_config.module_name_for m }
+
+        # Generate digestified paths from the modules spec
+        paths = {}
+        modules.each { |m| paths[m] = _javascript_path(m).sub /\.js$/,'' }
+
+        if run_config.has_key? 'paths'
+          # Add paths for assets specified by full URL (on a CDN)
+          run_config['paths'].each { |k,v| paths[k] = v if v =~ /^https?:/ }
+        end
+
+        # Override user paths, whose mappings are only relevant in dev mode
+        # and in the build_config.
+        run_config['paths'] = paths
+      end
+      "<script>var require = #{run_config.to_json};</script>".html_safe
+    end
+  end
 
   def requirejs_include_tag(name=nil, &block)
     requirejs = Rails.application.config.requirejs
@@ -31,39 +59,12 @@ module RequirejsHelper
     html = ""
 
     _once_guard do
-      unless requirejs.run_config.empty?
-        run_config = requirejs.run_config.dup
-        unless _priority.empty?
-          run_config = run_config.dup
-          run_config[:priority] ||= []
-          run_config[:priority].concat _priority
-        end
-        if Rails.application.config.assets.digest
-          modules = requirejs.build_config['modules'].map { |m| requirejs.module_name_for m }
 
-          # Generate digestified paths from the modules spec
-          paths = {}
-          modules.each { |m| paths[m] = _javascript_path(m).sub /\.js$/,'' }
+      html.concat requirejs
+      
+      html.concat "<script #{_requirejs_data(name, &block)} src=\"#{_javascript_path 'require.js'}\"></script>".html_safe
 
-          if run_config.has_key? 'paths'
-            # Add paths for assets specified by full URL (on a CDN)
-            run_config['paths'].each { |k,v| paths[k] = v if v =~ /^https?:/ }
-          end
-
-          # Override user paths, whose mappings are only relevant in dev mode
-          # and in the build_config.
-          run_config['paths'] = paths
-        end
-        html.concat <<-HTML
-        <script>var require = #{run_config.to_json};</script>
-        HTML
-      end
-
-      html.concat <<-HTML
-      <script #{_requirejs_data(name, &block)} src="#{_javascript_path 'require.js'}"></script>
-      HTML
-
-      html.html_safe
+      html
     end
   end
 
